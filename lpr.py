b97f461fa5aa304e12cae85b4e2c0580edde8549
ปฏิยุทธ์ เพชรฤทธิ์ <31218538+trangservice@users.noreply.github.com>
2022-08-09 21:09:50 +0700

    Update lpr.py

--- lpr.py
+++ lpr.py
@@ -10,5 +10,4 @@ json_object = json.dumps(r.json(), indent=4, ensure_ascii=False)
 with open("data.json", "w", encoding='utf8') as outfile:
     outfile.write(json_object)
 
-
-
+# output >> r.headers (see rate limit), r.json() (box, `...)
